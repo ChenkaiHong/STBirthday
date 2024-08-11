043efcdcbff0e56b0191ea36eef2d55c162cb7de
ChenkaiHong <166433491+ChenkaiHong@users.noreply.github.com>
2024-08-10 23:57:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var password = "poke";
 
 function passcheck(){
-    if(document.getElementById('pass1').toLowerCase.value != password) {
+    if(document.getElementById('pass1').value != password) {
         alert('Go Fish ____ Bar')
         return false;
     }
-    if(document.getElementById('pass1').toLowerCase.value == password) {
+    if(document.getElementById('pass1').value == password) {
         alert('Nicee');
     }
 }
